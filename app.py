406bd5c3ebb700fce09ec0510fe766852a7e65e2
thigarette <thigamureithi@hotmail.com>
2019-12-06 16:23:22 +0300

    Initial Commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, FloatField, SelectField
+from wtforms.validators import DataRequired
+import model as bundas
+app = Flask(__name__)
+
+user_item_details = {}
+
+class PredictionForm(FlaskForm):
+    max_price = FloatField('Max Price', validators=[DataRequired])
+    fat_content = SelectField('Fat Content',
+                           choices=[('Low Fat', 'Low Fat'), ('Regular', 'Regular')])
+    visibility = FloatField('Visibility', validators=[DataRequired])
+    category = SelectField('Category',
+                           choices=[('Baking Goods', 'Baking Goods'),
+                                    ('Breads', 'Breads'),
+                                    ('Breakfast', 'Breakfast'),
+                                    ('Canned', 'Canned'),
+                                    ('Dairy', 'Dairy'),
+                                    ('Frozen Foods', 'Frozen Foods'),
+                                    ('Fruits and Vegetables', 'Fruits and Vegetables'),
+                                    ('Hard Drinks', 'Hard Drinks'),
+                                    ('Health and Hygiene', 'Health and Hygiene'),
+                                    ('Household', 'Household'),
+                                    ('Meat', 'Meat'),
+                                    ('Others', 'Others'),
+                                    ('Seafood', 'Seafood'),
+                                    ('Snack Foods', 'Snack Foods'),
+                                    ('Soft Drinks', 'Soft Drinks'),
+                                    ('Starchy Foods', 'Starchy Foods')])
+    store_size = SelectField('Store Size',
+                             choices=[('Small', 'Small'), ('Medium', 'Medium'), ('High', 'High'), ('Unknown', 'Unknown')])
+    store_location_type = SelectField('Store Location Type',
+                                      choices=[('Tier 1', 'Tier 1'), ('Tier 2', 'Tier 2'), ('Tier 3', 'Tier 3'), ('Grocery Store', 'Grocery Store')])
+    store_type = SelectField('Store Type',
+                             choices=[('Type1', 'Type1'), ('Type2', 'Type2'), ('Type3', 'Type3')])
+    submit = SubmitField('Make Prediction')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        max_price = request.form["max_price"]
+        fat_content = request.form["fat_content"]
+        visibility = request.form["visibility"]
+        category = request.form["category"]
+        store_size = request.form["store_size"]
+        store_location_type = request.form["store_location_type"]
+        store_type = request.form["store_type"]
+
+        user_item_details = {
+            "FatContent": [fat_content],
+            "Visibility": [visibility],
+            "Category": [category],
+            "Max_Price": [max_price],
+            "Store_Size": [store_size],
+            "Store_Location_Type": [store_location_type],
+            "Store_Type": [store_type]
+        }
+        # user_item_details.extend([visibility, max_price, fat_content, category, store_size, store_location_type, store_type])
+        encoded_item_details = bundas.preprocess_input(user_item_details)
+        sales_prediction = bundas.model_predict(encoded_item_details)
+        pred = sales_prediction[0].astype(str)
+        print(sales_prediction[0])
+        return "Sales Prediction is "+pred
+    form = PredictionForm()
+    return render_template('custom.html', form=form)
+
+
+if __name__ == '__main__':
+    app.secret_key = 'mysecret'
+    app.run(host='0.0.0.0', debug=True)
---
+++ model.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from sklearn.feature_selection import chi2
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn import linear_model
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
+bundas_train = pd.read_csv("bundas_train.csv")
+bundas_train['Store_Size'] = bundas_train.Store_Size.fillna('Unknown')
+
+weight = bundas_train[['Weight']]
+bundas_train[['Weight']] = bundas_train.Weight.fillna(float(weight.mean()))
+
+bundas_train.FatContent.replace('low fat', 'Low Fat', inplace=True)
+bundas_train.FatContent.replace('LF', 'Low Fat', inplace=True)
+bundas_train.FatContent.replace('reg', 'Regular', inplace=True)
+
+X_train = bundas_train.drop(['Item_ID', 'Weight', 'Store_Establishment_Year', 'Store_ID', 'Item_Store_Sales'], axis=1)
+y_train = bundas_train['Item_Store_Sales']
+
+X_train_features = pd.get_dummies(X_train)
+
+X_train_final, X_validation, y_train_final, y_validation = train_test_split(X_train_features, y_train, test_size=0.3)
+
+model = linear_model.LinearRegression()
+model.fit(X=X_train_final, y=y_train_final)
+y_pred = model.predict(X_validation)
+print(r2_score(y_validation, y_pred))
+
+
+def preprocess_input(user_item_details):
+    data = pd.DataFrame(user_item_details)
+    encoded_data = pd.get_dummies(data)
+    missing_cols = set(X_train_final.columns) - set(encoded_data.columns)
+
+    for column in missing_cols:
+        encoded_data[column] = 0
+    encoded_data = encoded_data[X_train_final.columns]
+    return encoded_data
+
+
+def model_predict(encoded_item_data):
+    item_sales_pred = model.predict(encoded_item_data)
+    return item_sales_pred
